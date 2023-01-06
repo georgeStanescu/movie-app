0c7f0b3fa518bb2c8b2a33fe3da15b0e86732dac
George Stanescu <georgestanescu@outlook.com>
2023-01-06 16:17:21 +0200

    add material ui

---
+++ resources/js/Pages/Movies.tsx
@@ -0,0 +1,17 @@
+import { Head } from '@inertiajs/inertia-react';
+import { Button, ButtonGroup, Grid } from '@mui/material';
+
+export default function Movies(props) {
+    return (
+        <>
+            <Head title="Movies" />
+            <Grid container justifyContent={'center'}>
+                <ButtonGroup variant="outlined" aria-label="outlined button group">
+                    <Button>One</Button>
+                    <Button>Two</Button>
+                    <Button>Three</Button>
+                </ButtonGroup>
+            </Grid >
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/movies', function () {
+    return Inertia::render('Movies', []);
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
